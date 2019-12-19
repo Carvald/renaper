0e02e0d903df1416c09f9d00565dd5c4bcf83333
Pablo Martin Viva <pablo.viva@bbva.com>
2019-12-19 15:30:40 -0300

    Actualizo aplicacion para generar war

--- src/main/java/ar/com/bbva/arq/esqueleto/EsqueletoApplication.java
+++ src/main/java/ar/com/bbva/arq/esqueleto/EsqueletoApplication.java
@@ -2,14 +2,20 @@ package ar.com.bbva.arq.esqueleto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"ar.com.bbva.arq.esqueleto", "ar.com.bbva.arq.spring.boot.utils"})
-public class EsqueletoApplication {
+public class EsqueletoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EsqueletoApplication.class, args);
 	}
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EsqueletoApplication.class);
+    }
 }
