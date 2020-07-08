8f88fd4e72d01e3e680cea8ec84156d7c92a7f90
cvalderrama <cvalderrama@tecnosoftware.com>
2020-07-08 17:10:33 -0300

    avance renaper api ultimos ajustes deploy en websphere

--- src/main/java/ar/com/bbva/arq/renaper/RenaperApplication.java
+++ src/main/java/ar/com/bbva/arq/renaper/RenaperApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+//import ar.com.bbva.arq.spring.boot.utils.JNDIProfileHelper;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"ar.com.bbva.arq.renaper", "ar.com.bbva.arq.spring.boot.utils"})
@@ -18,4 +19,14 @@ public class RenaperApplication extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(RenaperApplication.class);
     }
+	
+    /*@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // Estamos corriendo en un servlet container, Websphere application server posiblemente
+        // En estos casos cargamos el profile de Spring Boot mediante JNDI
+        JNDIProfileHelper.loadProfile();
+        return builder.sources(RenaperApplication.class);
+    }*/
+
+	
 }
